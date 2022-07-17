2799eed86afde3fec9434c396a8e93bae0ba0904
aranya <aranya.9545@gmail.com>
2022-07-18 01:44:27 +0700

    4.7-4.8

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -13,6 +13,7 @@ app.component('product-display',{
             <p v-if="inventory > 10">In Stock</p>
             <p v-else-if="inventory <= 10 && inventory > 0">In Stock</p>
             <p v-else>Out of Stock</p>
+            <p>Shipping: {{Shipping}}</p>
             <ul>
                 <li v-for="detail in details">{{ detail }}</li>
             </ul>
@@ -59,6 +60,12 @@ computed: {
     },
     inStock(){
         return this.variants[this.selectedVariant].quantity
+    },
+    shipping() {
+        if (this.premium) {
+            return 'Free'
+        }
+        return 30
     },
     onSale(){
         if(this.variants[this.selectedVariant].onSale){
